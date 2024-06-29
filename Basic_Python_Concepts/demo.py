1d8a3a4c57b06b660a68098f66c80bc6853cdefe
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-06-29 09:28:09 +0800

    Update demo.py

--- Basic_Python_Concepts/demo.py
+++ Basic_Python_Concepts/demo.py
@@ -4,4 +4,11 @@ print('Hey there')
 """June 29, 2024 
  Practice """
 
+firstname = input("Enter firstname: ")
+lastname = input("Enter lastname: ")
 
+username = firstname+lastname
+print(username)
+
+email = username+"@gmail.com"
+print(email)
