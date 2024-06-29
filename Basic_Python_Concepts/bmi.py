5d951dd8496063f81c07a5242cde35fa1850d32b
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-06-29 10:20:20 +0800

    Create bmi.py

---
+++ Basic_Python_Concepts/bmi.py
@@ -0,0 +1,10 @@
+"""BMI formula
+BMI = weight in kg / (height in m)^2
+"""
+
+weight = float(input("Your weight in kg?: "))
+height = float(input("Your height in m?: "))
+
+BMI = weight / height
+
+print(f"Your BMI is {BMI} kg/m^2")
