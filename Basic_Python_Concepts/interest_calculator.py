a00caa48d5372cf046c244ccd80b5e1ddc38ca8f
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-06-29 09:57:01 +0800

    Update interest_calculator.py

--- Basic_Python_Concepts/interest_calculator.py
+++ Basic_Python_Concepts/interest_calculator.py
@@ -11,4 +11,4 @@ years = float(input("Enter the period in years: "))
 rate = float(input("Enter rate of interest: "))
 
 interest = principal * years * rate / 100
-print("interest = ", interest)
+print("interest = "+ str(interest))
