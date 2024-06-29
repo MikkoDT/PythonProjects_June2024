d5c566c49ae15908a5e9ee194c7c08e95319801f
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-06-29 09:50:12 +0800

    Update interest_calculator.py

---
+++ Basic_Python_Concepts/interest_calculator.py
@@ -0,0 +1,14 @@
+"""Simple Interest Formula
+SI = P * N * R / 100
+P = principal
+N = years
+R = rate
+
+"""
+
+principal = int(input("Enter the amount borrowed: "))
+years = int(input("Enter the period in years: "))
+rate = int(input("Enter rate of interest: "))
+
+interest = principal * years * rate / 100
+print("interest = "+interest)
