5953ca1bfb08c83bf9bcb035eb82709b8a0e9f00
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-02 21:03:55 +0800

    update

---
+++ Basic_Python_Concepts/demo_Dictionary.py
@@ -0,0 +1,13 @@
+people = {"John":32,"Rob":40,"Tim":20}
+print(people)
+
+print(people["Rob"])
+
+print(people["Tim"])
+
+people_id = {1:"Ford",2:"Walton",3:"Parker"}
+print(people_id[2])
+
+people["Rob"] = 50
+
+print(people)
