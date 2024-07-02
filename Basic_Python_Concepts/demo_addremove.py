0101174e983c0f796f67a51ae2c779b77974e83b
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-02 22:04:38 +0800

    update

---
+++ Basic_Python_Concepts/demo_addremove.py
@@ -0,0 +1,10 @@
+setA = {1,2,3,4,5}
+
+setA.add(10)
+print(setA)
+
+setA.remove(2)
+print(setA)
+
+setA.discard(20)
+print(setA)
--- Basic_Python_Concepts/demo_setoperations.py
+++ Basic_Python_Concepts/demo_setoperations.py
@@ -4,4 +4,8 @@ print("Tim" in s)
 setA = {1,2,3,4,5}
 setB = {4,5,6,7,8}
 
-print(setA | setB)
+print(setA | setB) #Union
+print(setA & setB) #Intersection
+print(setA - setB) #Difference
+print(setB - setA) #Difference
+print(setA ^ setB) #Symetric Difference
