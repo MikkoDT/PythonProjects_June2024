a98a443c23f16ecc3abc947ffe68e93e120173e2
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-07 17:27:08 +0800

    Create demo_elif.py

---
+++ Basic_Python_Concepts/demo_elif.py
@@ -0,0 +1,14 @@
+marks = float(input('Enter marks: '))
+
+# >90 A, >70 B, >60 C, <60 D
+
+if marks>=90:
+    print('Grade A')
+elif marks>=70:
+    print('Grade B')
+elif marks>=60:
+    print('Grade C')
+elif marks<=60:
+    print('Grade D')
+
+
