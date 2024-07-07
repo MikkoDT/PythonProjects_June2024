a790a931bd55377b69f18e655318961ecc8ec642
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-07 17:35:33 +0800

    update

---
+++ Basic_Python_Concepts/dem_NestedIf.py
@@ -0,0 +1,10 @@
+a = 10
+b = 20
+
+if a>5:
+    if b>15:
+        print('A is greater than 5 & B is greater than 15')
+    else:
+        print('B is not greater than 15, but A greater than 5')
+else:
+    print('A is not greater than 5')
