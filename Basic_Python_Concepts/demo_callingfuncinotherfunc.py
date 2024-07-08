8e80ec3ee4f71bee0d23cf0a7e2ec66c68c59c3c
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-08 16:31:30 +0800

    update

--- Basic_Python_Concepts/demo_callingfuncinotherfunc.py
+++ Basic_Python_Concepts/demo_callingfuncinotherfunc.py
@@ -6,6 +6,4 @@ def cost(circle_area,cost_per_sqm):
     total_cost = circle_area*cost_per_sqm
     return total_cost
 
-calculated_area = area(10,3.14)
-tc = cost(calculated_area,2)
-print(tc)
+print(cost(area(10,3.14),2))
