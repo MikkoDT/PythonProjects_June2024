b59c7ccee2e01658dbb264f3f7fe5d9d14bc396b
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-08 16:37:56 +0800

    update

--- Basic_Python_Concepts/demo_funcreturn_multiplevalues.py
+++ Basic_Python_Concepts/demo_funcreturn_multiplevalues.py
@@ -1,4 +1,8 @@
 def circle(r):
     area = 3.14 * r * r
     circumference = 2 * 3.14 * r
-    return area
+    return area,circumference
+
+a,c = circle(10)
+
+print(f"Area of the circle is {a}, circumference of the circle i {c}.")
