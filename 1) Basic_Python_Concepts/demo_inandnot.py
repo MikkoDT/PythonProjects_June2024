70f591e93ce167a476f9c3d949d85abbadf28b77
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-09 09:57:49 +0800

    update

---
+++ 1) Basic_Python_Concepts/demo_inandnot.py
@@ -0,0 +1,7 @@
+fruits = ['apple','mango','peach','orange','watermelon','grape']
+
+print('apple' in fruits)
+
+print('pineapple' not in fruits)
+
+print('peach' not in fruits)
--- Basic_Python_Concepts/demo_inandnot.py
+++
@@ -1,7 +0,0 @@
-fruits = ['apple','mango','peach','orange','watermelon','grape']
-
-print('apple' in fruits)
-
-print('pineapple' not in fruits)
-
-print('peach' not in fruits)
