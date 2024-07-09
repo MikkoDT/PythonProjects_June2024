a1507b0b705ec643218081648268eea31dd2d550
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-09 15:02:06 +0800

    Create EMI_calculator.py

---
+++ 4) Functions/EMI_calculator.py
@@ -0,0 +1,5 @@
+#formula to calculate EMI is: P x R x (1+R)^N / [(1+R)^N-1]
+
+def emi_calculator(principal,rate,time):
+    r = rate/12/100
+    emi = (principal * r * (1+r)**time) / ((1+r)**time - 1)
