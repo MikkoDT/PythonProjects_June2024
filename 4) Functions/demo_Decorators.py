bf14006ac34f3c36a0570833105a71ca0b28770b
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-09 20:55:27 +0800

    Update demo_Decorators.py

---
+++ 4) Functions/demo_Decorators.py
@@ -0,0 +1,9 @@
+def chocolate():
+    print("Chocolate")
+
+def decorator(func):
+    def wrapper():
+        print("Wrapper up side")
+        func()
+        print("Wrapper down side")
+    return wrapper
