c641c57845db05b79b0d28d2752a58ed9bdf92a9
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-09 20:51:25 +0800

    Update demo_variablelength_2.py

--- 4) Functions/demo_variablelength_2.py
+++ 4) Functions/demo_variablelength_2.py
@@ -1,2 +1,8 @@
-def product():
-    
+def product(**kwargs):
+    for key,value in kwargs.items():
+        print(key+":"+value)
+
+
+
+product(name="iphone",price="700")
+product(name="ipad",price="400",description="This is an ipad")
