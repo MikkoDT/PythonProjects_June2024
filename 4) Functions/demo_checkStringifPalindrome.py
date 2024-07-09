244c8b8e2e79c0d3b460dde102790f329ad385ee
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-09 14:46:43 +0800

    Update demo_checkStringifPalindrome.py

---
+++ 4) Functions/demo_checkStringifPalindrome.py
@@ -0,0 +1,18 @@
+word = "python"
+#print(word[5])
+
+l = len(word)
+
+#Looping through the letters
+palindrome_flag = True
+for i in range(l):
+    if word[i] != word[l-i-1]:
+        palindrome_flag = False
+        break
+    else:
+        palindrome_flag = True
+
+if palindrome_flag:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
