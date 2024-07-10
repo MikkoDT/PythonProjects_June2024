3978a4de17e2932102b6e98e24ec80009d1bbc1a
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-10 21:40:29 +0800

    Update demo_elseblock.py

---
+++ 6) Errors and Exceptions/demo_elseblock.py
@@ -0,0 +1,12 @@
+n = int(input('Enter numerator: '))
+d = int(input('Enter denominator: '))
+
+result = 0
+
+try:
+    result = n/d
+except ZeroDivisionError:
+    print('Cannot divide a number by zero.')
+
+else:
+    print(result)
