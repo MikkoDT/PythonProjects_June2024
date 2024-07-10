6854430ed3de40ba3af2deb892d59b1df4592c37
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-10 21:37:26 +0800

    update

---
+++ 6) Errors and Exceptions/demo_tryExcept.py
@@ -0,0 +1,11 @@
+n = int(input('Enter numerator: '))
+d = int(input('Enter denominator: '))
+
+result = 0
+
+try:
+    result = n/d
+except ZeroDivisionError:
+    print('Cannot divide a number by zero.')
+
+print(result)
