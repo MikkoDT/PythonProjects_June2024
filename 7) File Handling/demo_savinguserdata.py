0831b2f7e8c3862e25af331d3ca5c689028f4518
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-12 21:49:31 +0800

    update

---
+++ 7) File Handling/demo_savinguserdata.py
@@ -0,0 +1,9 @@
+while True:
+    with open('names.txt',"a") as file:
+        name = input("Enter name to be added: ")
+        file.write(name+'\n')
+        choice = input("Do you want to add more names? (y/n) ")
+        if choice == 'n':
+            break
+
+
--- 7) File Handling/demo_strip.py
+++ 7) File Handling/demo_strip.py
@@ -1,3 +1,10 @@
 text = "  hello world!  "
 stripped_text = text.strip()
 print(stripped_text)
+
+with open('data.txt','r') as file:
+    lines = file.readlines()
+
+print(lines)
+for line in lines:
+    print(line.strip())
