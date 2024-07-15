eb0cf92d4419f78a9828a30d33ef61a361bdb19c
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-15 20:42:35 +0800

    Update demo_savingcomplex.py

--- 7) File Handling/demo_savingcomplex.py
+++ 7) File Handling/demo_savingcomplex.py
@@ -7,4 +7,8 @@ def save_user_data():
     with open("user_data.txt","a") as file:
         file.write(user_data)
 
-save_user_data()
+def read_user_data():
+    with open("user_data.txt","r") as file:
+        print(file.read())
+
+read_user_data()
