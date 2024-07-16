3ba32bbac1e271adc08ae4ce6965975a9712ea91
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-16 21:23:55 +0800

    Update demo_lambdaFunc.py

--- 8) Lambdas, Map and Filter/demo_lambdaFunc.py
+++ 8) Lambdas, Map and Filter/demo_lambdaFunc.py
@@ -5,5 +5,8 @@ def square(x):
 print(square(9))
 """
 
-result = (lambda x: x**2)(7)
-print(result)
+result1 = (lambda x: x**2)(7)
+print("result1 = ",result1)
+
+result2 = (lambda x,y: x+y)(2,3)
+print("result2 = ",result2)
