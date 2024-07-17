c43eb96e6968f84bef2b2dd3533b024b3dc1df6b
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-07-17 20:33:05 +0800

    Update demo_mapUses.py

--- 8) Lambdas, Map and Filter/demo_mapUses.py
+++ 8) Lambdas, Map and Filter/demo_mapUses.py
@@ -8,3 +8,11 @@ prices = [100,200,300,400,500]
 
 new_prices = list(map(lambda x: x - x*5/100,prices))
 print(new_prices)
+
+names = ['john','rob','mike']
+
+cap_names = list(map(str.capitalize,names))
+print(cap_names)
+
+cap_names = list(map(str.upper,names))
+print(cap_names)
