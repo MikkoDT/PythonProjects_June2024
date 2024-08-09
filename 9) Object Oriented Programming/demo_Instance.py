9bbfabfddf39c1fa9706c486991ad71ac62ac6c8
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-09 20:04:06 +0800

    update

--- 9) Object Oriented Programming/demo_Instance.py
+++ 9) Object Oriented Programming/demo_Instance.py
@@ -2,4 +2,14 @@ class Product:
     quantity = 200
 
     def __init__(self,name,price):
-        
+        self.name = name
+        self.price = price
+
+
+p1 = Product("phone","300")
+print(p1.name)
+print(p1.price)
+
+p2 = Product("laptop","900")
+print(p2.name)
+print(p2.price)
