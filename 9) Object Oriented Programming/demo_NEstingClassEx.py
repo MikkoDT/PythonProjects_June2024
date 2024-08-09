6778f8ab870541c3ee415ffbbf3d8f998cdd2413
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-09 22:03:53 +0800

    Update demo_NEstingClassEx.py

--- 9) Object Oriented Programming/demo_NEstingClassEx.py
+++ 9) Object Oriented Programming/demo_NEstingClassEx.py
@@ -1,2 +1,26 @@
 class Zoo:
-    
+    def __init__(self):
+        self.animals = []
+
+    def add_animal(self,name,species):
+        animal = self.Animal(name,species)
+        self.animals.append(animal)
+
+    class Animal:
+        def __init__(self,name,species):
+            self.name = name
+            self.species = species
+
+        def display_info(self):
+            print(f"Name:{self.name}, Species:{self.species}")
+
+#creating a zoo
+my_Zoo = Zoo()
+
+#add animals to the zoo
+my_Zoo.add_animal("Lion","Mammal")
+my_Zoo.add_animal("Eagle","Bird")
+my_Zoo.add_animal("Crocodile","Reptile")
+
+for animal in my_Zoo.animals:
+    animal.display_info()
