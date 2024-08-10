009acdd4e25cced295487467417311170d896b6f
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-10 20:57:35 +0800

    Update demo_combine_shorthand_and_metacharacters.py

--- 10) Regular Expressions in Python/demo_combine_shorthand_and_metacharacters.py
+++ 10) Regular Expressions in Python/demo_combine_shorthand_and_metacharacters.py
@@ -1,5 +1,7 @@
 import re
 text = "Helloooooo, Python is awesomeeeee!"
 pattern = r"\w*o+\w*"
+#\w*: Matches zero or more alphanumeric characters
+#o+: Matches one or more occurrences of the letter 'o'
 matches = re.findall(pattern,text)
 print(matches)
