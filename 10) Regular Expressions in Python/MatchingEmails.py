d616674e19f3aab8c389d41c3ca186271ddc4062
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-10 21:23:46 +0800

    Update MatchingEmails.py

--- 10) Regular Expressions in Python/MatchingEmails.py
+++ 10) Regular Expressions in Python/MatchingEmails.py
@@ -1,5 +1,5 @@
 import re
-text = "Please contact me at john@example.co or via email at jane@example.com"
+text = "Please contact me at john@example.com or via email at jane@example.com"
 # john@example.com
 
 pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
