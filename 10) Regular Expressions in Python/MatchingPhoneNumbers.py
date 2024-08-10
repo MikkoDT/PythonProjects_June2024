21adb6b26cba49b1edfec8985077919c8e25a9a9
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-10 21:11:56 +0800

    update

--- 10) Regular Expressions in Python/MatchingPhoneNumbers.py
+++ 10) Regular Expressions in Python/MatchingPhoneNumbers.py
@@ -1,8 +1,8 @@
 import re
-text = "Please contact me at +1 (123) 456-7890 or via email at john@example.com"
+text = "Please contact me at +91-123-456-7890 or via email at john@example.com"
 # +1 (123) 456-7890
 
-pattern = r"\+?\d{1,3}[-.\s]\(?\d{1,3}\)?"
+pattern = r"\+?\d{1,3}[-.\s]\(?\d{1,3}\)?[-.\s]?\d{1,3}[-.\s]?\d{1,4}"
 
 matches = re.findall(pattern,text)
 print(matches)
