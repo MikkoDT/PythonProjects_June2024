ef4dc76691ed5f221cec9328b000356d6376be14
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-10 22:03:43 +0800

    Update demo_button.py

--- 10) Tkinter/demo_button.py
+++ 10) Tkinter/demo_button.py
@@ -1,8 +1,14 @@
 from tkinter import *
 
+def display():
+    print('Button clicked')
+
 root = Tk()
 root.geometry("300x300")
-button = Button(root,text="Click here")
+button = Button(root,text="Click here",command=display)
 button.pack()
+
+
+
 
 root.mainloop()
