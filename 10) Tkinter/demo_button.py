153f40ce9f76c4059196b1ab421258f1af4d0e04
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-10 22:02:03 +0800

    update

--- 10) Tkinter/demo_button.py
+++ 10) Tkinter/demo_button.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.geometry("300x300")
-hello = Label(root,text="Hello World")
-hello.pack()
+button = Button(root,text="Click here")
+button.pack()
 
 root.mainloop()
