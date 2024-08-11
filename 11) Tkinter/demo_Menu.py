75a6d8949c0b641b7b99e615d29b6e5030c61484
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-11 15:03:51 +0800

    Update demo_Menu.py

--- 11) Tkinter/demo_Menu.py
+++ 11) Tkinter/demo_Menu.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+def function1():
+    print("Menu item clicked.")
+
 root = Tk()
 my_menu = Menu(root)
 root.config(menu=my_menu)
@@ -7,5 +10,8 @@ root.config(menu=my_menu)
 submenu = Menu(my_menu)
 
 my_menu.add_cascade(label='File',menu=submenu)
+
+submenu.add_command(label='Project',command=function1)
+submenu.add_command(label='Save',command=function1)
 
 root.mainloop()
