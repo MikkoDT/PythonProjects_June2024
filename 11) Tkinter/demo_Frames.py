c55f33ba45a010883e2cd6b6ae131c22a14cd433
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-11 11:32:09 +0800

    Update demo_Frames.py

--- 11) Tkinter/demo_Frames.py
+++ 11) Tkinter/demo_Frames.py
@@ -1,6 +1,18 @@
 from tkinter import *
 
 root = Tk()
+frame = Frame(root)
+frame.pack()
+
+frame2 = Frame(root)
+frame2.pack()
+
+button = Button(frame,text="Button1")
+button2 = Button(frame2,text="Button2")
+
+button.pack()
+button2.pack()
+
 root.geometry("300x300")
 
 
