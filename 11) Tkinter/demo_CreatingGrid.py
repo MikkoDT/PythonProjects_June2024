18b2e48b2d37d803051c26ccbf205da78d422abf
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-11 11:51:15 +0800

    Update demo_CreatingGrid.py

--- 11) Tkinter/demo_CreatingGrid.py
+++ 11) Tkinter/demo_CreatingGrid.py
@@ -1,7 +1,11 @@
 from tkinter import *
 
 root = Tk()
-root.geometry("300x300")
+
+for x in range(3):
+    for y in range(3):
+        frame = Frame(root)
+        frame.grid(row=x,column=y)
 
 
 root.mainloop()
